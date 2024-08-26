16b3fd34adbf723bc702c0ac72fcc9abc80671ec
Frank Mwito <Mfranklyne039@gmail.com>
2024-08-26 09:56:17 +0300

    factorial of a number

--- numberFactorial.py
+++ numberFactorial.py
@@ -1,1 +1,11 @@
 # calculate the factorial of a number
+
+number = int(input("Enter any number:"))
+factorial = 1
+i = 1
+
+while i <= number:
+    factorial *= i
+    i += 1
+    
+print(factorial)
