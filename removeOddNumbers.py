77a87b1368fadc4d9cdfcef3b6bf976af0f3012e
Frank Mwito <Mfranklyne039@gmail.com>
2024-08-26 10:16:34 +0300

    remove odd numbers from a list

--- removeOddNumbers.py
+++ removeOddNumbers.py
@@ -1,1 +1,18 @@
 # remove odd numbers from a list
+
+num = int(input("Enter any number for size of list:"))
+numbers = list()
+
+for i in range(num):
+    numbers.append(int(input("Enter any number:")))
+    
+print(numbers)
+
+i = 0
+while i < len(numbers):
+    if numbers[i] % 2 != 0:
+        numbers.pop(i)
+    else:
+        i += 1
+        
+print(numbers)
